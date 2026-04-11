69cd89cac71b739c05caa8c5e474786267eba8ef
Zairex-Code <dylanyesidflorez@gmail.com>
2026-04-11 06:49:14 -0500

    feature/ bd.sql

---
+++ connection/bd.sql
@@ -0,0 +1,23 @@
+
+CREATE DATABASE italo_components;
+USE italo_components;
+
+CREATE TABLE inventory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category varchar(100) NOT null,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT DEFAULT 0
+);
+
+CREATE TABLE sales (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    inventory_id INT,
+    customer_name VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL,
+    sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_inventory_sales 
+    FOREIGN KEY (inventory_id) REFERENCES inventory(id)
+);
+
+
